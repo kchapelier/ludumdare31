8cca72470bb6b8eb0c8f0e04e807bad53b42befe
kchapelier <kevin.chapelier@gmail.com>
2014-12-06 10:17:26 +0100

    fix potential bug in position (all object sharing the same vectors)

--- src/game/components/position.js
+++ src/game/components/position.js
@@ -4,8 +4,15 @@ module.exports = {
     x : 0,
     y : 0,
     speed : 250,
-    direction : new Victor(0, 0),
-    directionIntent : new Victor(0, 0),
+    initialize : function(element) {
+        if(!element.direction) {
+            element.direction = new Victor(0, 0);
+        }
+
+        if(!element.directionIntent) {
+            element.directionIntent = new Victor(0, 0);
+        }
+    },
     update : function(element, dt) {
         var mult = 1000 / element.speed;
         element.x+= element.directionIntent.x * dt / mult;
